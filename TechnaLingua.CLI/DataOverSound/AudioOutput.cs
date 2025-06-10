7efe4a9c196f40066d4b87f1da671446219db300
Altair-sh <altairgyg@gmail.com>
2025-06-11 04:46:51 +0500

    created class AudioOutput

---
+++ TechnaLingua.CLI/DataOverSound/AudioOutput.cs
@@ -0,0 +1,89 @@
+﻿using ggwave.net;
+using ggwave.net.Native;
+using NAudio.Sdl2;
+using NAudio.Sdl2.Interop;
+using NAudio.Sdl2.Structures;
+using NAudio.Wave;
+
+namespace TechnaLingua.CLI.DataOverSound;
+
+public class AudioOutput : IDisposable
+{
+    public readonly WaveOutSdlCapabilities DeviceCapabilities;
+    public readonly int SamplesPerFrame;
+    public int VolumePercent;
+    
+    private readonly WaveOutSdl _outputDevice;
+    private readonly WaveFormat _waveFormat;
+    private readonly GGWaveInstance _ggWave;
+
+    public AudioOutput(WaveOutSdl outputDevice, WaveOutSdlCapabilities deviceCapabilities, 
+        int samplesPerFrame = 256, int volumePercent = 30)
+    {
+        _outputDevice = outputDevice;
+        DeviceCapabilities = deviceCapabilities;
+        SamplesPerFrame = samplesPerFrame;
+        VolumePercent = volumePercent;
+
+        var ggWaveParams = GGWaveStatic.getDefaultParameters();
+        ggWaveParams.sampleRate = deviceCapabilities.Frequency;
+        ggWaveParams.sampleRateOut = deviceCapabilities.Frequency;
+        (ggWaveParams.sampleFormatOut, _waveFormat) = deviceCapabilities.Format switch
+        {
+            SDL.AUDIO_U16 => (GGWaveSampleFormat.FORMAT_U16, 
+                new WaveFormat(deviceCapabilities.Frequency, 16, 1)),
+            SDL.AUDIO_S16 => (GGWaveSampleFormat.FORMAT_I16, 
+                new WaveFormat(deviceCapabilities.Frequency, 16, 1)),
+            SDL.AUDIO_F32 => (GGWaveSampleFormat.FORMAT_F32, 
+                WaveFormat.CreateIeeeFloatWaveFormat(deviceCapabilities.Frequency, 1)),
+            _ => throw new ArgumentOutOfRangeException(
+                nameof(deviceCapabilities.Format), 
+                deviceCapabilities.Format.ToString())
+        };
+        ggWaveParams.samplesPerFrame = samplesPerFrame;
+        Console.WriteLine($"GGWave params: {ggWaveParams}");
+        _ggWave = new GGWaveInstance(ggWaveParams);
+    }
+    
+    public async Task EncodeAndPlay(Stream dataReadStream)
+    {
+        var playbackStoppedCts = new CancellationTokenSource();
+        _outputDevice.PlaybackStopped += (s, e) =>
+        {
+            if (e.Exception is not null)
+                throw new AggregateException(e.Exception);
+            playbackStoppedCts.Cancel();
+        };
+
+        var waveStream = new ProducerConsumerStream();
+
+        var waveWriter = waveStream.GetWriteStream();
+        Console.WriteLine("Encoding");
+        _ggWave.EncodeStream(dataReadStream, waveWriter, 
+            GGWaveProtocolId.AUDIBLE_NORMAL, VolumePercent);
+        Console.WriteLine("Done");
+
+        var waveReader = new RawSourceWaveStream(waveStream.CreateReadStream(), _waveFormat);
+        Console.WriteLine("Init out device");
+        _outputDevice.Init(waveReader);
+        Console.WriteLine("Playback started");
+        _outputDevice.Play();
+        
+        Console.WriteLine("Waiting for playback stop");
+        while(!playbackStoppedCts.Token.IsCancellationRequested)
+        {
+            // ReSharper disable once MethodSupportsCancellation
+            await Task.Delay(100);
+        }
+        // wait until device actually stops playing audio
+        // ReSharper disable once MethodSupportsCancellation
+        await Task.Delay(500);
+        Console.WriteLine("Playback stopped");
+    }
+
+    public void Dispose()
+    {
+        _outputDevice.Dispose();
+        _ggWave.Dispose();
+    }
+}
--- TechnaLingua.CLI/Program.cs
+++ TechnaLingua.CLI/Program.cs
@@ -1,16 +1,14 @@
 ﻿using System.Text;
-using ggwave.net;
-using ggwave.net.Native;
 using NAudio.Sdl2;
 using NAudio.Sdl2.Interop;
 using NAudio.Sdl2.Structures;
-using NAudio.Wave;
+using TechnaLingua.CLI.DataOverSound;
 
 namespace TechnaLingua.CLI;
 
 class Program
 {
-    static (WaveOutSdl, WaveOutSdlCapabilities)  GetOutputDevice()
+    static (WaveOutSdl, WaveOutSdlCapabilities) GetOutputDevice()
     {
         var audioOutputDevices = WaveOutSdl.GetCapabilitiesList();
         foreach (WaveOutSdlCapabilities device in audioOutputDevices)
@@ -18,11 +16,11 @@ class Program
             Console.WriteLine(device.ToString(",\n\t"));
         }
         
-        // Console.Write("Select output device number: ");
-        // string? deviceIdStr = Console.ReadLine();
+        Console.Write("Select output device number: ");
+        string? deviceIdStr = Console.ReadLine();
         int deviceId = 0;
-        // if(!string.IsNullOrWhiteSpace(deviceIdStr))
-        //     deviceId = Convert.ToInt32(deviceIdStr);
+        if(!string.IsNullOrWhiteSpace(deviceIdStr))
+            deviceId = Convert.ToInt32(deviceIdStr);
         var deviceCapabilities = audioOutputDevices[deviceId];
         Console.WriteLine($"Selected output device: {deviceCapabilities.DeviceName}");
         if(!deviceCapabilities.IsAudioCapabilitiesValid)
@@ -36,9 +34,9 @@ class Program
         return (outputDevice, deviceCapabilities);
     }
 
-    static string GetTextInput() => "Prived";
+    static string GetTextInput() => "Превед медвед";
 
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         SDL.SDL_GetVersion(out var sdlVersion);
         Console.WriteLine($"SDL Version: {sdlVersion.major}.{sdlVersion.minor}.{sdlVersion.patch}");
@@ -46,84 +44,14 @@ class Program
         var text = GetTextInput();
         var utf8WithoutBom = new UTF8Encoding(encoderShouldEmitUTF8Identifier:false, throwOnInvalidBytes: true);
         var dataBytes = utf8WithoutBom.GetBytes(text);
-        // using var inputDataStream = new ProducerConsumerStream();
-        // inputDataStream.Write(dataBytes, 0, dataBytes.Length);
-        
+        await using var inputDataStream = new ProducerConsumerStream();
+        inputDataStream.GetWriteStream().Write(dataBytes, 0, dataBytes.Length);
+        var dataReadStream = inputDataStream.CreateReadStream();
+
         var (outputDevice, outputDeviceCapabilities) = GetOutputDevice();
-
-        var ggWaveParams = GGWaveStatic.getDefaultParameters();
-        ggWaveParams.sampleRate = outputDeviceCapabilities.Frequency;
-        (ggWaveParams.sampleFormatOut, WaveFormat waveFormat) = outputDeviceCapabilities.Format switch
+        using (AudioOutput audioOutput = new(outputDevice, outputDeviceCapabilities))
         {
-            SDL.AUDIO_U16 => (GGWaveSampleFormat.FORMAT_U16, 
-                new WaveFormat(outputDeviceCapabilities.Frequency, 16, 1)),
-            SDL.AUDIO_S16 => (GGWaveSampleFormat.FORMAT_I16, 
-                new WaveFormat(outputDeviceCapabilities.Frequency, 16, 1)),
-            SDL.AUDIO_F32 => (GGWaveSampleFormat.FORMAT_F32, 
-                WaveFormat.CreateIeeeFloatWaveFormat(outputDeviceCapabilities.Frequency, 1)),
-            _ => throw new ArgumentOutOfRangeException(
-                nameof(outputDeviceCapabilities.Format), 
-                outputDeviceCapabilities.Format.ToString())
-        };
-        Console.WriteLine($"GGWave params: {ggWaveParams}");
-        using var ggWave = new GGWaveInstance(ggWaveParams);
-
-        // using var pcmOutStream = new ProducerConsumerStream();
-        // var waveBuffer = new BufferedWaveProvider(waveFormat)
-        // {
-        //     DiscardOnBufferOverflow = true
-        // };
-
-        var playbackStoppedCts = new CancellationTokenSource();
-        outputDevice.PlaybackStopped += (s, e) =>
-        {
-            Console.WriteLine($"Playback stopped ({e.Exception})");
-            playbackStoppedCts.Cancel();
-        };
-
-        byte[] dataPcm = ggWave.Encode(
-            dataBytes,
-            GGWaveProtocolId.AUDIBLE_NORMAL,
-            50);
-        var waveStream = new ProducerConsumerStream();
-        using (var waveFileWriter = new WaveFileWriter(waveStream.GetWriteStream(), waveFormat))
-        {
-            waveFileWriter.Write(dataPcm, 0, dataPcm.Length);
-        }
-        
-        using (var waveFileReader = new WaveFileReader(waveStream.CreateReadStream()))
-        {
-            outputDevice.Init(waveFileReader);
-            outputDevice.Play();
-
-            // Console.WriteLine("WritePcmDataToBufferAsync");
-            // WritePcmDataToBufferAsync(pcmOutStream, waveBuffer);
-            // Console.WriteLine("Encoding");
-            // ggWave.EncodeStream(inputDataStream, pcmOutStream, 
-            //     GGWaveProtocolId.AUDIBLE_NORMAL, 50);
-            // Console.WriteLine("Done");
-
-            playbackStoppedCts.Token.WaitHandle.WaitOne();
-            Thread.Sleep(2000);
-        }
-    }
-    
-    static async void WritePcmDataToBufferAsync(Stream pcmOutStream, BufferedWaveProvider waveBuffer)
-    {
-        try
-        {
-            byte[] buffer = new byte[4096];
-            int bytesRead;
-
-            while ((bytesRead = await pcmOutStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
-            {
-                Console.WriteLine($"Add sample {bytesRead} bytes");
-                waveBuffer.AddSamples(buffer, 0, bytesRead);
-            }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.ToString());
+            await audioOutput.EncodeAndPlay(dataReadStream);
         }
     }
 }
